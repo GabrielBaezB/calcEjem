c2df688fbc3c3d01c8fd296a5ce0571fc0e92147
GabrielBaezB <gabooo@live.com>
2020-09-01 19:02:54 -0400

    Solucionado

--- Fuentes/CalculadoraApp/app/src/main/java/com/inacap/calculadoraapp/MainActivity.java
+++ Fuentes/CalculadoraApp/app/src/main/java/com/inacap/calculadoraapp/MainActivity.java
@@ -4,18 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText n1Txt;
+    private EditText n2Txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Aqui puedo acceder a los elementos de la interfez, referente del View que tiene el id n1_txt
+        this.n1Txt = findViewById(R.id.n1_txt);
+        this.n2Txt = findViewById(R.id.n2_txt);
     }
-    
-    public void saludar(View v){
 
-        Toast.makeText(this, "Hola mundo", Toast.LENGTH_SHORT).show();
+    private double calcular(int n1, int n2, String op)throws Exception {
+    double resultado = 0;
+    switch (op){
+        case "suma" : resultado = n1 + n2;
+        break;
+        case "resta": resultado = n1 - n2;
+        break;
+        case "multiplicacion": resultado = n1 * n2;
+        break;
+        case "division" : resultado = n1 / n2;
+        break;
+        default:throw new Exception();
+    }return resultado;
+}
+
+    public void sumar(View v){
+        int n1, n2;
+        n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        try{
+        double suma =  calcular(n1, n2, "suma");
+        Toast.makeText(this, "La Suma es "+ suma, Toast.LENGTH_SHORT).show();
+    }catch (Exception ex){
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void restar(View v){
+        int n1, n2;
+        n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        try{
+            double resta =  calcular(n1, n2, "resta");
+            Toast.makeText(this, "La resta es "+ resta, Toast.LENGTH_SHORT).show();
+        }catch (Exception ex){
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void multiplicar(View v){
+        int n1, n2;
+        n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        try{
+            double multiplicacion =  calcular(n1, n2, "multiplicacion");
+            Toast.makeText(this, "La multiplicacion es "+ multiplicacion, Toast.LENGTH_SHORT).show();
+        }catch (Exception ex){
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void dividir(View v){
+        int n1, n2;
+        n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        n2 = Integer.parseInt(this.n2Txt.getText().toString());
+        try{
+            double division =  calcular(n1, n2, "division");
+            Toast.makeText(this, "La division es "+ division, Toast.LENGTH_SHORT).show();
+        }catch (Exception ex){
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
